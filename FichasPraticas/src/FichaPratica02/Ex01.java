5e2a3877a2adbd57b3eff2c3b1364a4be1a8b198
Vitor Santos <vmvs007@gmail.com>
2025-11-07 09:39:11 +0000

    FichaPratica02

--- FichasPraticas/src/FichaPratica02/Ex01.java
+++ FichasPraticas/src/FichaPratica02/Ex01.java
@@ -1,4 +1,28 @@
 package FichaPratica02;
 
+import java.util.Scanner;
+
 public class Ex01 {
+    public static void main(String[] args) {
+
+        // Import do Scanner
+        Scanner input = new Scanner(System.in);
+
+        // Declarar as variáveis
+        int num1, num2;
+
+        // Ler números
+        System.out.print("Insira o 1º número: ");
+        num1 = input.nextInt();
+
+        System.out.print("Insira o 2º número: ");
+        num2 = input.nextInt();
+
+        if (num1 > num2) {
+            System.out.println("Maior: " + num1);
+        } else {
+            System.out.println("Maior: " + num2);
+        }
+
+    }
 }
